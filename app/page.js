1bf0f7cfff3c46d48d5022f00819b47e9be40e78
Tarik-Khalfi <khtarek07@gmail.com>
2023-10-05 10:57:55 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,18 @@
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import SpecialDish from "./components/SpecialDish";
+import Video from "./components/Video";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Main />
+      <SpecialDish />
+      <Menu />
+      <Video />
+    </main>
+  );
+}
